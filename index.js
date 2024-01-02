e3256037d3c7700f4533fa8b40afe2338575b7dc
ImAadarsh <aadarshkavita@gmail.com>
2024-01-03 00:50:38 +0530

    AA

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('./db/conn');
 const express=require('express');
 const cors=require('cors');
-const port = process.env.PORT || 5011;
+const port = process.env.PORT || 5000;
 const app=express();
 const Coupon = require('./models/Coupan');
 const Category = require('./models/Category');
